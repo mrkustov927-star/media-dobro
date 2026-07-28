b6d5e04dc0d5262d837dc61856af4e0bb9cf4b82
mrkustov927-star <mrkustov927@gmail.com>
2026-07-28 19:40:56 +0300

    Подключить уведомления ВК в кабинете организатора

--- dobro-media-cabinet/app/admin-blagotvori/layout.tsx
+++ dobro-media-cabinet/app/admin-blagotvori/layout.tsx
@@ -3,11 +3,13 @@
 import type { ReactNode } from 'react';
 import './admin-mobile-controls.css';
 import AdminVacancyTeamEnhancer from './AdminVacancyTeamEnhancer';
+import AdminVkNotificationEnhancer from './AdminVkNotificationEnhancer';
 
 export default function AdminBlagotvoriLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <AdminVacancyTeamEnhancer />
+      <AdminVkNotificationEnhancer />
       {children}
     </>
   );
