861b1ae126b00a42c6dfaad597ee5a9d71ffeb41
mrkustov927-star <mrkustov927@gmail.com>
2026-07-28 11:04:55 +0300

    Подключить мобильные элементы управления кабинета

--- dobro-media-cabinet/app/admin-blagotvori/layout.tsx
+++ dobro-media-cabinet/app/admin-blagotvori/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { ReactNode } from 'react';
+import './admin-mobile-controls.css';
 import AdminVacancyTeamEnhancer from './AdminVacancyTeamEnhancer';
 
 export default function AdminBlagotvoriLayout({ children }: { children: ReactNode }) {
