14e9a3417c9ccfa99a12d80754cffdb50977e54b
mrkustov927-star <mrkustov927@gmail.com>
2026-08-05 20:41:29 +0300

    Enable August media vacancy auto-installer

--- dobro-media-cabinet/app/admin-blagotvori/layout.tsx
+++ dobro-media-cabinet/app/admin-blagotvori/layout.tsx
@@ -3,12 +3,14 @@
 import type { ReactNode } from 'react';
 import AdminVacancyTeamEnhancer from './AdminVacancyTeamEnhancer';
 import AdminVacancyDeleteEnhancer from './AdminVacancyDeleteEnhancer';
+import AdminAugustMediaAutoInstaller from './AdminAugustMediaAutoInstaller';
 
 export default function AdminBlagotvoriLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <AdminVacancyTeamEnhancer />
       <AdminVacancyDeleteEnhancer />
+      <AdminAugustMediaAutoInstaller />
       {children}
     </>
   );
