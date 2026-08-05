f2cce1627d42f6c707c07943f61c66d7ce03ab15
mrkustov927-star <mrkustov927@gmail.com>
2026-08-05 19:30:35 +0300

    Подключить пакет августовских карточек в админке

--- dobro-media-cabinet/app/admin-blagotvori/layout.tsx
+++ dobro-media-cabinet/app/admin-blagotvori/layout.tsx
@@ -2,11 +2,13 @@
 
 import type { ReactNode } from 'react';
 import AdminVacancyTeamEnhancer from './AdminVacancyTeamEnhancer';
+import AdminAugustActionsEnhancer from './AdminAugustActionsEnhancer';
 
 export default function AdminBlagotvoriLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <AdminVacancyTeamEnhancer />
+      <AdminAugustActionsEnhancer />
       {children}
     </>
   );
